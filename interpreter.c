3654f1e7351ff0d5e022bc8e3398884a51c9b55a
Stefan Eilers <stefan.eilers@intelligentmobiles.com>
2024-03-31 22:30:39 +0200

    Demonstration of a crashing program.

--- interpreter.c
+++ interpreter.c
@@ -7,6 +7,22 @@
 
 int main(int argc, char **argv)
 {
-	printf("this is a test\n");
+	printf("this is a test!\n");
+	printf("This will crash when printf is called multiple times!\n");
+	printf("This will crash when printf is called multiple times!\n");
+	printf("This will crash when printf is called multiple times!\n");
+	printf("This will crash when printf is called multiple times!\n");
+	printf("This will crash when printf is called multiple times!\n");
+	printf("This will crash when printf is called multiple times!\n");
+	printf("This will crash when printf is called multiple times!\n");
+	printf("This will crash when printf is called multiple times!\n");
+	printf("This will crash when printf is called multiple times!\n");
+	printf("This will crash when printf is called multiple times!\n");
+	printf("This will crash when printf is called multiple times!\n");
+	printf("This will crash when printf is called multiple times!\n");
+	printf("This will crash when printf is called multiple times!\n");
+	printf("This will crash when printf is called multiple times!\n");
+	printf("This will crash when printf is called multiple times!\n");
+	printf("This will crash when printf is called multiple times!\n");
 	return 0;
 }
