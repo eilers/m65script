64fa555a9bd78f4a6a41cae8e9967331ae1b377c
Stefan Eilers <stefan.eilers@intelligentmobiles.com>
2024-05-08 18:28:58 +0200

    Try to fully control the keyboard input on mac.

--- unix/m65script_conio.c
+++ unix/m65script_conio.c
@@ -19,7 +19,10 @@ void enableRawMode() {
     tcgetattr(STDIN_FILENO, &orig_termios);
     atexit(disableRawMode);
     struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+    raw.c_iflag &= ~(ICRNL | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
